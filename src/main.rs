a15cc5ba591316a1133b47dfa9cb81a7d1f52811
Gerald Lee Yurek III <g3@yurek.me>
2025-07-05 19:37:07 -0400

    util functions file and command matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+mod util;
+use clap::{Arg, Command, Subcommand, ValueEnum};
+use serde;
+use serde_yml;
+use util::{Config, build_command, get_config_path};
+
+use crate::util::{read_config, write_default_config};
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let command_struct = build_command(Command::new("file-rustler"));
+
+    let command_matches = command_struct.get_matches();
+
+    let config_dir = get_config_path();
+    let config: Config = read_config(config_dir.to_str().unwrap())?;
+
+    match command_matches.subcommand() {
+        Some(("run", sub_matches)) => {}
+        Some(("sim", sub_matches)) => {}
+        Some(("new", sub_matches)) => {
+            write_default_config();
+        }
+        Some(("edit", sub_matches)) => {
+            open::that(config_dir)?;
+        }
+        Some(("check", sub_matches)) => {}
+        Some(("debug", sub_matches)) => {}
+        Some(("show", sub_matches)) => {}
+        _ => {}
+    }
+    Ok(())
 }
---
+++ src/util.rs
@@ -0,0 +1,77 @@
+use clap::Command;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::io::{self, Read};
+use std::path::{Path, PathBuf};
+#[derive(Deserialize, Debug)]
+pub struct NameFilter {
+    begins_with: String,
+    contains: Vec<String>,
+    ends_with: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Filter {
+    name: NameFilter,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Rule {
+    name: String,
+    locations: Vec<String>,
+    filters: Vec<Filter>,
+}
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    rules: Vec<Rule>,
+}
+
+pub fn read_config_file(path: &str) -> Result<String, Box<dyn Error>> {
+    let path = shellexpand::tilde(path).to_string();
+    fs::read_to_string(&path).map_err(Into::into)
+}
+
+pub fn read_config(path: &str) -> Result<Config, Box<dyn Error>> {
+    let config_content = read_config_file(path)?;
+    let config: Config = serde_yml::from_str(&config_content)?;
+    Ok(config)
+}
+
+pub fn get_config_path() -> PathBuf {
+    env::var("XDG_CONFIG_HOME")
+        .map(PathBuf::from)
+        .unwrap_or_else(|_| {
+            env::home_dir()
+                .map(|home| home.join(".config"))
+                .unwrap_or_else(|| PathBuf::from(".config"))
+        })
+        .join("file-rustler")
+        .join("config.yaml")
+}
+
+pub fn build_command(cmd: Command) -> Command {
+    cmd.author("Gerald Lee Yurek III, g3@yurek.me")
+        .version("0.0.1")
+        .about("A program that organizes files based on a YAML config file")
+        .subcommand(Command::new("run").about("Organize files"))
+        .subcommand(Command::new("sim").about("Simulate organizing files"))
+        .subcommand(Command::new("new").about("Create a YAML template config file"))
+        .subcommand(Command::new("edit").about("Edit the YAML config file with $EDITOR"))
+        .subcommand(Command::new("check").about("Check validity of Config file"))
+        .subcommand(Command::new("debug").about("Shows the raw config parsing steps"))
+        .subcommand(Command::new("show").about("Print the config file to stdout"))
+}
+
+pub fn write_default_config() {
+    let default_config = r#"
+# Name / Explanation of rule
+#--------------------------------------------------------
+rule: "Lorem Ipsum"
+locations: 
+      - ~/Downloads
+
+"#;
+}
